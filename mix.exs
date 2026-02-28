f0898b6ead456122775d270afdfdacba7efe12b5
Alex Shneyderman <a.shneyderman@gmail.com>
2026-02-28 11:15:47 -0500

    chore: update so we can compile without any compilation warnings

--- mix.exs
+++ mix.exs
@@ -2,13 +2,28 @@ defmodule Matrix.Mixfile do
   use Mix.Project
 
   def project do
-    [app: :matrix,
-     version: "0.3.1",
-     description: description(),
-     package: package(),
-     elixir: "~> 1.1",
-     deps: deps(),
-     docs: [extras: []]]
+    [
+      app: :matrix,
+      version: "0.3.1",
+      description: """
+      Matrix is a linear algebra library for manipulating dense matrices. Its
+      primary design goal is ease of use.
+      """,
+      package: [
+        maintainers: ["Tom Krauss"],
+        licenses: ["Apache 2.0"],
+        links: %{
+          "GitHub" => "https://github.com/twist-vector/elixir-matrix.git",
+          "Docs" => "http://http://hexdocs.pm/matrix"
+        }
+      ],
+      elixir: "~> 1.1",
+      deps: [
+        {:ex_doc, "~> 0.14", only: :dev},
+        {:exprintf, "~> 0.2"}
+      ],
+      docs: [extras: []]
+    ]
   end
 
   # Configuration for the OTP application
@@ -17,25 +32,4 @@ defmodule Matrix.Mixfile do
   def application do
     [applications: [:logger, :exprintf]]
   end
-
-  defp deps do
-    [{:ex_doc, "~> 0.14", only: :dev},
-     {:exprintf, "~> 0.1"}]
-  end
-
-  defp description do
-    """
-    Matrix is a linear algebra library for manipulating dense matrices. Its
-    primary design goal is ease of use.
-    """
-  end
-
-  defp package do
-    [# These are the default files included in the package
-     maintainers: ["Tom Krauss"],
-     licenses: ["Apache 2.0"],
-     links: %{"GitHub" => "https://github.com/twist-vector/elixir-matrix.git",
-              "Docs" => "http://http://hexdocs.pm/matrix"}]
-  end
-
 end
